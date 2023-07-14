77afc0f10cf4d1a88b4ab5043325535e3e93f8a4
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-13 22:08:55 -0500

    Create App.jsx

---
+++ Portafolio/src/App.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "../src/components/Navbar.jsx";
+import Footer from "../src/components/Footer.jsx";
+import Home from "../src/pages/Home.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div id="root" className="flex flex-col min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+export default App;
