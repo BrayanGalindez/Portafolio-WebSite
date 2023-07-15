bc57aba1ccd4c39888a3db3be58bba3b06eb72f4
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-14 20:33:14 -0500

    Update Footer.jsx

--- Portafolio/src/components/Footer.jsx
+++ Portafolio/src/components/Footer.jsx
@@ -3,18 +3,18 @@ import { CiLocationOn } from 'react-icons/ci';
 
 function Footer() {
   return (
-    <footer className="p-5 bg-slate-600 shadow md:flex md:item-center md:justify-between bottom-0 w-full relative">
+    <footer className="p-5 bg-white dark:bg-gray-900 shadow md:flex md:item-center md:justify-between bottom-0 w-full relative">
       <div className="flex justify-between items-center flex-grow">
         <div className="flex items-center flex-grow">
-          <a className="text-3xl ml-2">
+          <a className="text-3xl ml-2 text-gray-800 dark:text-white dark:hover:text-red-500 hover:text-red-500 duration-500">
             <CiLocationOn />
           </a>
-          <a>Colombia</a>
+          <a className='text-xl text-gray-800 dark:text-white'>Colombia-Bogota</a>
         </div>
         <div className="flex">
           <a
             href="https://github.com/BrayanGalindez"
-            className="text-3xl cursor-pointer ml-2"
+            className="text-3xl cursor-pointer ml-2 text-gray-800 dark:text-white dark:hover:text-purple-500 hover:text-purple-500 duration-500"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -22,7 +22,7 @@ function Footer() {
           </a>
           <a
             href="https://www.linkedin.com/in/brayangalindez24/"
-            className="text-3xl cursor-pointer ml-2"
+            className="text-3xl cursor-pointer ml-2 text-gray-800 dark:text-white dark:hover:text-cyan-500 hover:text-cyan-500 duration-500"
             target="_blank"
             rel="noopener noreferrer"
           >
