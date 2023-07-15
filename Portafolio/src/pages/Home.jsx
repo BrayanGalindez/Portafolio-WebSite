6800bf71cd5a727d95e95f838c48f799b30ff17b
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-14 20:33:32 -0500

    Update Home.jsx

--- Portafolio/src/pages/Home.jsx
+++ Portafolio/src/pages/Home.jsx
@@ -1,28 +1,27 @@
-import About from '../components/About';
-import Skills from '../components/Skills';
-import Projects from '../components/Projects';
-import Education from '../components/Education';
-import Contact from '../components/Contact';
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Education from "../components/Education";
+import Contact from "../components/Contact";
 function Home() {
   return (
-    
-    <div className="bg-gray-200 flex-grow">
-      <div className="mb-8">
-        <About />
+      <div className="bg-gray-200 flex-grow">
+        <div >
+          <About />
+        </div>
+        <div >
+          <Skills />
+        </div>
+        <div >
+          <Projects />
+        </div>
+        <div >
+          <Education />
+        </div>
+        <div >
+          <Contact />
+        </div>
       </div>
-      <div className="mb-8">
-        <Skills />
-      </div>
-      <div className="mb-8">
-        <Projects />
-      </div>
-      <div className="mb-8">
-        <Education />
-      </div>
-      <div className="mb-8">
-        <Contact />
-      </div>
-    </div>
   );
 }
 
