6e3933817a71070ffe56c918adcb256cb0425606
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-15 16:12:02 -0500

    Update App.jsx

--- Portafolio/src/App.jsx
+++ Portafolio/src/App.jsx
@@ -1,20 +1,45 @@
-// import React from 'react'
+import { useRef } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "../src/components/Navbar.jsx";
 import Footer from "../src/components/Footer.jsx";
 import Home from "../src/pages/Home.jsx";
 
 function App() {
+  // Definir las referencias de los componentes
+  const aboutRef = useRef(null);
+  const skillsRef = useRef(null);
+  const projectsRef = useRef(null);
+  const educationRef = useRef(null);
+  const contactRef = useRef(null);
+
   return (
-        <BrowserRouter>
-          <div id="root" className="flex flex-col min-h-screen">
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-            <Footer />
-          </div>
-        </BrowserRouter>
+    <BrowserRouter>
+      <div id="root" className="flex flex-col min-h-screen">
+        <Navbar
+          aboutRef={aboutRef}
+          skillsRef={skillsRef}
+          projectsRef={projectsRef}
+          educationRef={educationRef}
+          contactRef={contactRef}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                aboutRef={aboutRef}
+                projectsRef={projectsRef}
+                skillsRef={skillsRef}
+                educationRef={educationRef}
+                contactRef={contactRef}
+              />
+            }
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
+
 export default App;
