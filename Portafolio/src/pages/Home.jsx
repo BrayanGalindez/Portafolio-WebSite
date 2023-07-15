de60a812d924f43825e6f6090328646cb30ea8b4
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-15 16:12:17 -0500

    Update Home.jsx

--- Portafolio/src/pages/Home.jsx
+++ Portafolio/src/pages/Home.jsx
@@ -1,28 +1,39 @@
+import PropTypes from "prop-types";
+
 import About from "../components/About";
-import Skills from "../components/Skills";
-import Projects from "../components/Projects";
-import Education from "../components/Education";
-import Contact from "../components/Contact";
-function Home() {
+import Skills from "../components/Skills.jsx";
+import Projects from "../components/Projects.jsx";
+import Education from "../components/Education.jsx";
+import Contact from "../components/Contact.jsx";
+
+function Home({ aboutRef, projectsRef, skillsRef, contactRef, educationRef }) {
   return (
-      <div className="bg-gray-200 flex-grow">
-        <div >
-          <About />
-        </div>
-        <div >
-          <Skills />
-        </div>
-        <div >
-          <Projects />
-        </div>
-        <div >
-          <Education />
-        </div>
-        <div >
-          <Contact />
-        </div>
+    <div className="bg-white dark:bg-gray-900 flex-grow">
+      <div ref={aboutRef}>
+        <About />
       </div>
+      <div className="mt-16" ref={skillsRef}>
+        <Skills />
+      </div>
+
+      <div ref={projectsRef}>
+        <Projects />
+      </div>
+
+      <div ref={educationRef}>
+        <Education />
+      </div>
+      <div ref={contactRef}>
+        <Contact />
+      </div>
+    </div>
   );
 }
-
+Home.propTypes = {
+  aboutRef: PropTypes.object,
+  projectsRef: PropTypes.object,
+  skillsRef: PropTypes.object,
+  educationRef: PropTypes.object,
+  contactRef: PropTypes.object,
+};
 export default Home;
