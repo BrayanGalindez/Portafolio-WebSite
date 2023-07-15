10d97ceb16d845864f91a1824683f7d9b2b92337
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-14 20:32:58 -0500

    Update About.jsx

--- Portafolio/src/components/About.jsx
+++ Portafolio/src/components/About.jsx
@@ -1,25 +1,24 @@
-// import React from 'react';
 import foto from "../assets//About/perfil.png";
+import { useContext } from "react";
+import { LanguageContext } from "../providers/Context.jsx";
+import dataAbout from "../components/data/DataAbout.jsx";
+
 function About() {
+  const { language } = useContext(LanguageContext);
+  const { greeting, description, button } = dataAbout[language];
+
   return (
-    <section id="about" className="bg-gray-200 py-8">
+    <section id="about" className="bg-white py-8 dark:bg-gray-900 mt-10">
       <div className="max-w-5xl mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="max-w-5xl mx-auto py-8">
-          <h2 className="text-3xl font-bold text-gray-800 mb-4">
-            Hola, soy Brayan Galindez
+          <h2 className="text-3xl font-bold text-gray-800 dark:text-white mb-4">
+            {greeting}
           </h2>
-          <p className="text-xl text-gray-700 mb-4">
-            Soy desarrollador web y actualmente estoy estudiando Ingeniería de
-            Sistemas y Computación. Poseo habilidades en el desarrollo
-            FullStack, lo que me permite crear sitios web funcionales en todas
-            sus capas, desde el frontend hasta el backend. Estoy constantemente
-            aprendiendo y mejorando mis habilidades para estar al tanto de las
-            últimas tecnologías y tendencias en el campo del desarrollo web.
-            Estoy emocionado de poner en práctica mis habilidades y
-            conocimientos en proyectos emocionantes y desafiantes.
+          <p className="text-xl text-gray-700 dark:text-white mb-4">
+            {description}
           </p>
-          <button className="bg-slate-600 text-white font-[Poppins] duration-500 px-6 py-2 hover:bg-slate-700 rounded">
-            Hoja de vida
+          <button className="bg-blue-500 dark:bg-indigo-600 text-white dark:text-white font-[Poppins] duration-500 px-6 py-2 hover:bg-slate-700 rounded">
+            {button}
           </button>
         </div>
         <div className="bg-white rounded-lg shadow-lg w-64 h-64 mt-20">
