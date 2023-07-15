e9fa439e6683d1a9ac23a6390c6283b20ab45677
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-14 20:33:12 -0500

    Update Education.jsx

--- Portafolio/src/components/Education.jsx
+++ Portafolio/src/components/Education.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { BsFileEarmarkText } from "react-icons/bs";
-import Certificado_1 from "../assets/Education/Mintic.pdf";
-import Certificado_2 from "../assets/Education/DS4A.pdf";
-import Certificado_3 from "../assets/Education/InnovationCampus.pdf";
+import { useContext } from "react";
+import { LanguageContext } from "../providers/Context.jsx";
+import dataEducation from "../components/data/DataEducation.jsx";
+
+
 function Education() {
+  const { language } = useContext(LanguageContext);
+  const education = dataEducation[language];
   const [showMore, setShowMore] = useState({});
 
   const handleShowMore = (index) => {
@@ -19,107 +23,59 @@ function Education() {
       [index]: false,
     }));
   };
-  const certifications = [
-    {
-      title: "MisionTic",
-      description: (
-        <div>
-          <p>
-            MisionTIC es un programa de formación ofrecido por el Ministerio de
-            Tecnologías de la Información y las Comunicaciones (MinTIC) que se
-            enfoca en brindar capacitación en áreas fundamentales de la
-            programación. Durante este programa, los participantes adquieren
-            conocimientos y habilidades en temas como Fundamentos de
-            Programación, Programación Básica, Desarrollo de Software y
-            Desarrollo de Aplicaciones Web.
-          </p>
-          <p>
-            El programa MisionTIC tiene una duración total de 800 horas, durante
-            las cuales los participantes tienen la oportunidad de sumergirse en
-            el mundo de la programación y desarrollar proyectos prácticos que
-            les permiten aplicar los conceptos aprendidos.
-          </p>
-        </div>
-      ),
-      certificateLink: Certificado_1,
-    },
-    {
-      title: "Fundamentos en analitica de datos",
-      description: (
-        <div>
-          <p>
-            Durante el curso de Fundamentos en Analítica de Datos de Correlation
-            One, se adquirieron conocimientos en alfabetización de datos, Excel,
-            Python, estadística y limpieza de datos. Además, se exploraron temas
-            avanzados como visualización de datos, tablas dinámicas en Excel,
-            lógica condicional y diseño de tableros de control. El curso también
-            cubrió análisis logístico, bases de datos, SQL y organización de
-            equipos profesionales de datos en empresas. En resumen, el curso
-            proporcionó una sólida base en analítica de datos, abarcando desde
-            conceptos básicos hasta herramientas y técnicas avanzadas.
-          </p>
-        </div>
-      ),
-      certificateLink: Certificado_2,
-    },
-    {
-      title: "Curso en codificacion y programacion en python",
-      description: (
-        <div>
-          <p>
-            En el curso de programación en Python de Innovation Campus, se
-            enfocó en los fundamentos de Python con énfasis en análisis de
-            datos.
-          </p>
-        </div>
-      ),
-      certificateLink: Certificado_3,
-    },
-    // Agrega más objetos de certificación aquí
-  ];
+
+
 
   return (
-    <section id="education" className="py-8">
+    <section id="education" className="py-8 bg-white dark:bg-gray-900 ">
       <div className="max-w-5xl mx-auto">
-        <h2 className="text-3xl font-bold text-gray-800 mb-4">Educación</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {certifications.map((certification, index) => (
+        <h2 className="text-3xl font-bold text-gray-800 mb-4 dark:text-white">Educación</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
+          {education.map((certification, index) => (
             <div
               key={index}
-              className={`bg-white rounded-lg shadow-lg p-4 transition-transform duration-300 transform hover:scale-105 ${
+              className={`bg-gray-200 dark:bg-indigo-950 rounded-lg shadow-lg p-4 transition-transform duration-300 transform hover:scale-105 ${
                 showMore[index] ? "expanded" : ""
               }`}
             >
-              <h3 className="text-xl font-bold text-gray-800 mb-2">
+              <h3 className="text-xl font-bold text-gray-800 dark:text-white mb-2">
                 {certification.title}
               </h3>
+              {showMore[index] ? null : (
+                <img
+                  src={certification.imageFont}
+                  alt="Certificado"
+                  className="h-16 float-right ml-auto mb-2 opacity-70"
+                />
+              )}
               {showMore[index] ? (
                 <>
-                  <p className="text-gray-700 mb-4">
+                  <p className="text-gray-700 dark:text-white mb-4">
                     {certification.description}
                   </p>
+
                   <a
                     href={certification.certificateLink}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex items-center text-blue-500 hover:text-blue-600"
+                    className="flex items-center text-blue-500 dark:text-white dark:hover:text-blue-600 hover:text-blue-600"
                   >
                     <BsFileEarmarkText className="w-6 h-6 mr-2" />
-                    Ver certificado
+                    {language === "en" ? "View Certificate" : "Ver certificado"}
                   </a>
                   <button
                     onClick={() => handleShowLess(index)}
-                    className="bg-slate-600 text-white font-bold px-4 py-2 rounded hover:bg-slate-700 mt-4"
+                    className="bg-blue-500 dark:bg-indigo-600 text-white font-bold px-4 py-2 rounded hover:bg-slate-700 mt-4"
                   >
-                    Ver menos
+                    {language === "en" ? "Show Less" : "Ver menos"}
                   </button>
                 </>
               ) : (
                 <button
                   onClick={() => handleShowMore(index)}
-                  className="bg-slate-600 text-white font-bold px-4 py-2 rounded hover:bg-slate-700 mt-4"
+                  className="bg-blue-500 dark:bg-indigo-600 text-white font-bold px-4 py-2 rounded hover:bg-slate-700 mt-4"
                 >
-                  Ver más
+                  {language === "en" ? "Show More" : "Ver más"}
                 </button>
               )}
             </div>
