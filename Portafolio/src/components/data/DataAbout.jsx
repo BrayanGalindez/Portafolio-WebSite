aeee97c2b36bfaf8bb51820c268e89097daa88f8
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-14 20:33:02 -0500

    Create DataAbout.jsx

---
+++ Portafolio/src/components/data/DataAbout.jsx
@@ -0,0 +1,14 @@
+const dataAbout = {
+    es: {
+      greeting: "Hola, soy Brayan Galindez",
+      description: "Soy desarrollador web y actualmente estoy estudiando Ingeniería de Sistemas y Computación. Poseo habilidades en el desarrollo FullStack, lo que me permite crear sitios web funcionales en todas sus capas, desde el frontend hasta el backend. Estoy constantemente aprendiendo y mejorando mis habilidades para estar al tanto de las últimas tecnologías y tendencias en el campo del desarrollo web. Estoy emocionado de poner en práctica mis habilidades y conocimientos en proyectos emocionantes y desafiantes.",
+      button: "Hoja de vida",
+    },
+    en: {
+      greeting: "Hello, I'm Brayan Galindez",
+      description: "I am a web developer and I am currently studying Systems Engineering and Computing. I have skills in FullStack development, which allows me to create functional websites in all its layers, from the frontend to the backend. I am constantly learning and improving my skills to stay on top of the latest technologies and trends in the field of web development. I am excited to put my skills and knowledge to use in exciting and challenging projects.",
+      button: "CV",
+    },
+  };
+  
+  export default dataAbout;
