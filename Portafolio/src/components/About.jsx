b49b98dddd0c26311b70110610d89841b6858793
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-17 19:29:24 -0500

    Update About.jsx

--- Portafolio/src/components/About.jsx
+++ Portafolio/src/components/About.jsx
@@ -14,26 +14,25 @@ function About() {
   const scrollToSection = () => {
     sectionRef.current.scrollIntoView({ behavior: "smooth" });
   };
-
-
-  return (
-    <section >
-      <section id="about" className="bg-white py-8 dark:bg-gray-900 ">
-        <div className="max-w-5xl mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="max-w-5xl mx-auto py-8">
-            <h2 className="text-3xl font-bold text-gray-800 dark:text-white mb-4">
-              {greeting}
-            </h2>
-            <p className="text-xl text-gray-700 dark:text-white mb-4">
-              {description}
-            </p>
-            <button
-              className="bg-blue-500 dark:bg-indigo-600 text-white dark:text-white font-[Poppins] duration-500 px-6 py-2 hover:bg-slate-700 rounded"
-              onClick={scrollToSection}
-            >
-              {button}
-            </button>
-          </div>
+return (
+  <section>
+    <section id="about" className="bg-white py-8 dark:bg-gray-900">
+      <div className="max-w-5xl mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="max-w-full mx-auto py-8 md:py-0">
+          <h2 className="text-3xl font-bold text-gray-800 dark:text-white mb-4">
+            {greeting}
+          </h2>
+          <p className="text-xl text-gray-700 dark:text-white mb-4">
+            {description}
+          </p>
+          <button
+            className="bg-blue-500 dark:bg-indigo-600 dark:hover:bg-indigo-700 text-white dark:text-white font-[Poppins] duration-500 px-6 py-2 hover:bg-slate-700 rounded"
+            onClick={scrollToSection}
+          >
+            {button}
+          </button>
+        </div>
+        <div className="max-w-full mx-auto py-8 md:py-0">
           {/* <div className="bg-white rounded-lg shadow-lg w-64 h-64 mt-20">
             <div className="h-64 flex items-center justify-center">
               <img src={foto} alt="Mi foto" className="rounded-t-lg " />
@@ -43,9 +42,10 @@ function About() {
             </div>
           </div> */}
         </div>
-      </section>
+      </div>
     </section>
-  );
+  </section>
+);
 }
 
 export default About;
