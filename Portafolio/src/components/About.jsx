0f342d2ae584d7da27b03f626617ec0105178fb8
BrayanGalindez <bgalindez@unal.edu.co>
2023-08-02 13:36:29 -0500

    [FIX] Se arregla el boton para mostrar la hoja de vida

--- Portafolio/src/components/About.jsx
+++ Portafolio/src/components/About.jsx
@@ -1,39 +1,40 @@
 // import foto from "../assets/About/perfil.png";
-import { useContext, useRef} from "react";
+import { useContext } from "react";
 import { LanguageContext } from "../providers/Context.jsx";
 import dataAbout_es from "./data/DataAbout/DataAbout_es.json";
 import dataAbout_en from "./data/DataAbout/DataAbout_en.json";
-
+import cv from "../assets/About/cv.pdf";
 function About() {
   const { language } = useContext(LanguageContext);
   const dataSkills = language === "es" ? dataAbout_es.es : dataAbout_en.en;
   const { greeting, description, button } = dataSkills;
 
-  const sectionRef = useRef(null);
-
-  const scrollToSection = () => {
-    sectionRef.current.scrollIntoView({ behavior: "smooth" });
+  const openCV = () => {
+    window.open(cv, "_blank");
   };
-return (
-  <section>
-    <section id="about" className="bg-white py-8 dark:bg-gray-900">
-      <div className="max-w-5xl mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="max-w-full mx-auto py-8 md:py-0">
-          <h2 className="text-3xl font-bold text-gray-800 dark:text-white mb-4">
-            {greeting}
-          </h2>
-          <p className="text-xl text-gray-700 dark:text-white mb-4">
-            {description}
-          </p>
-          <button
-            className="bg-blue-500 dark:bg-indigo-600 dark:hover:bg-indigo-700 text-white dark:text-white font-[Poppins] duration-500 px-6 py-2 hover:bg-slate-700 rounded"
-            onClick={scrollToSection}
-          >
-            {button}
-          </button>
-        </div>
-        <div className="max-w-full mx-auto py-8 md:py-0">
-          {/* <div className="bg-white rounded-lg shadow-lg w-64 h-64 mt-20">
+
+  return (
+    <section>
+      <section id="about" className="bg-white py-8 dark:bg-gray-900">
+        <div className="max-w-5xl mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="max-w-full mx-auto py-8 md:py-0">
+            <h2 className="text-3xl font-bold text-gray-800 dark:text-white mb-4">
+              {greeting}
+            </h2>
+            <p className="text-xl text-gray-700 dark:text-white mb-4">
+              {description}
+            </p>
+            <button
+             type="submit"
+             className="bg-blue-500 dark:bg-indigo-600 dark:hover:bg-indigo-700 text-white dark:text-white font-[Poppins] duration-500 px-6 py-2 hover:bg-slate-700 rounded"
+             onClick={openCV}
+             >
+              {button}
+              
+            </button>
+          </div>
+          <div className="max-w-full mx-auto py-8 md:py-0">
+            {/* <div className="bg-white rounded-lg shadow-lg w-64 h-64 mt-20">
             <div className="h-64 flex items-center justify-center">
               <img src={foto} alt="Mi foto" className="rounded-t-lg " />
             </div>
@@ -41,11 +42,11 @@ return (
             <div className="p-4">
             </div>
           </div> */}
+          </div>
         </div>
-      </div>
+      </section>
     </section>
-  </section>
-);
+  );
 }
 
 export default About;
